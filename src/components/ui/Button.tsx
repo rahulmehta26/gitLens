07a18289ae3c1786f253016a4bfee1e8349193dd
rahulmehta26 <rk6658542@gmail.com>
2026-04-20 23:00:08 +0530

    create reusable animated Button component with variants

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,50 @@
+import { type ReactNode } from "react";
+import { cn } from "../../utils/cn";
+import { motion, type HTMLMotionProps } from "motion/react"
+
+type Variant = "primary" | "outline" | "danger" | "ghost";
+
+interface ButtonProps extends HTMLMotionProps<"button"> {
+    children: ReactNode;
+    className?: string;
+    variant?: Variant;
+}
+
+const variants: Record<Variant, string> = {
+
+    primary: "bg-text-dim hover:bg-secondary border-white",
+    outline: "bg-transparent border-text-dim hover:border-lime",
+    danger: "border-danger hover:border-danger-secondary bg-danger-secondary hover:bg-danger",
+    ghost: ""
+}
+
+const Button: React.FC<ButtonProps> = (
+    ({ children, variant = "primary", className, ...props }) => (
+        <motion.button
+            className={cn(
+                "py-3 overflow-hidden relative px-6",
+                "cursor-pointer ",
+                "transition-all duration-300 ease-in-out",
+                "font-semibold hover:shadow-none custom-shadow",
+                variant === "ghost" ? " shadow-none p-0" : "border-2 rounded-full",
+                className,
+                variants[variant],
+            )}
+            {...props}
+
+            initial="initial"
+            whileHover="hover"
+        >
+
+            <motion.span
+                className="flex text-white items-center gap-3"
+                whileHover="hover"
+            >
+                {children}
+            </motion.span>
+
+        </motion.button>
+    )
+);
+
+export default Button;
