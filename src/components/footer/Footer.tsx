dc181e42b02124487fc80a419da75caec11f5b5b
rahulmehta26 <rk6658542@gmail.com>
2026-05-05 01:00:11 +0530

    make footer  responsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import OptimizedImage from '../ui/OptimizedImage'
 import footerImage from "../../assets/footer-image.png"
 import { useNavigate } from 'react-router'
 import { moveRight } from '../animations/buttonAnimations'
-import gitlensLogo from "../../assets/gitlens_logo.png"
+import { cn } from "../../utils/cn"
 
 const Footer = () => {
 
@@ -14,14 +14,19 @@ const Footer = () => {
         <footer className='w-full h-full bg-secondary'>
 
             <div
-                className='max-w-5xl min-h-screen flex flex-col mx-auto'
+                className='max-w-5xl min-h-screen lg:pt-12 flex flex-col mx-auto px-4 sm:px-8'
             >
 
-                <h2 className='font-display text-text-primary text-center text-[8.6rem] font-extrabold'>
+                <h2 className={cn(
+                    'font-display text-text-primary text-center font-extrabold',
+                    "text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[8.6rem]",
+                    "pt-12 lg:pt-0"
+
+                )}>
                     GITLENS
                 </h2>
 
-                <div className='mt-10 flex justify-center'>
+                <div className='mt-8 md:mt-10 flex justify-center'>
                     <Button
                         className='cursor-pointer'
                         onClick={() => navigate("/analyze")}
@@ -38,10 +43,15 @@ const Footer = () => {
                     </Button>
                 </div>
 
-                <div className='mt-auto flex justify-center gap-10 items-end'>
+                <div className={cn(
+                    'mt-20 md:mt-auto gap-6 md:gap-10',
+                    "flex flex-col sm:flex-row justify-center items-center sm:items-end",
+                    "pb-0"
+                )}
+                >
 
                     <div
-                        className=" flex gap-4 items-center  pb-6 "
+                        className="hidden md:flex gap-4 items-center pb-0 sm:pb-6 "
                     >
 
                         <span className='font-display text-text-primary text-sm font-extrabold'>
@@ -50,7 +60,7 @@ const Footer = () => {
                     </div>
 
 
-                    <div className='w-80 max-w-full'>
+                    <div className='w-48 sm:w-64 md:w-80 max-w-full'>
                         <OptimizedImage
                             src={footerImage}
                             alt='footer illustration image'
@@ -58,8 +68,17 @@ const Footer = () => {
                         />
                     </div>
 
+                    <div
+                        className="md:hidden flex gap-4 items-center pb-0 sm:pb-6 "
+                    >
+
+                        <span className='font-display text-text-primary text-sm font-extrabold'>
+                            &copy; gitLens {new Date().getFullYear()}
+                        </span>
+                    </div>
+
                     <span
-                        className='font-display text-center text-text-primary text-xs font-extrabold pb-6'
+                        className='font-display text-center text-text-primary text-xs font-extrabold pb-0 sm:pb-6'
                     >
                         WITH LOVES FOR <br />DEVELOPER
                     </span>
