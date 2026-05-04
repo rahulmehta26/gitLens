8b9e4f41047c0736ae6402e7fd1da7537a7a4013
rahulmehta26 <rk6658542@gmail.com>
2026-05-04 23:18:52 +0530

    add svg icon components

---
+++ src/components/icons/ErrorIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cn } from "../../utils/cn";
+
+const ErrorIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("icons", className)}
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path d="M12 9v4" />
+            <path d="M12 16v.01" />
+        </svg>
+    );
+};
+
+export default ErrorIcon;
