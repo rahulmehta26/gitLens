e05375ffbba923f5cd140664bd9402491b28d4b3
rahulmehta26 <rk6658542@gmail.com>
2026-05-04 23:29:58 +0530

    add routing with lazy-loaded pages and layout wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { lazy, Suspense } from 'react'
+import Layout from './components/layout/Layout';
+import { Route, Routes } from 'react-router-dom';
+
+const LandingPage = lazy(() => import("./pages/landing/LandingPage"));
+const AnalyzePage = lazy(() => import("./pages/AnalyzePage"));
+const RouteError = lazy(() => import("./pages/error/RouteError"));
 
 const App = () => {
   return (
-    <div>App</div>
+    <Suspense>
+      <Routes>
+        <Route element={<Layout />} >
+          <Route path='/' element={<LandingPage />} />
+          <Route
+            path="/analyze"
+            element={
+              <AnalyzePage />
+            }
+          />
+          <Route path='*' element={<RouteError />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
