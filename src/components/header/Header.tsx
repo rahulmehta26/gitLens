2d082637240d14706f58dc18432d18ff75ed38f1
rahulmehta26 <rk6658542@gmail.com>
2026-05-06 00:12:32 +0530

    make header responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,50 +6,48 @@ import { leftIconVariants, rightIconVariants } from "../animations/buttonAnimati
 import { useNavigate } from "react-router"
 import OptimizedImage from "../ui/OptimizedImage"
 import gitlensLogo from "../../assets/gitlens_logo.png"
+import { cn } from "../../utils/cn"
 
 const Header = () => {
 
     const navigate = useNavigate();
 
     return (
-        <header
-            className='fixed z-50 left-1/2 custom-shadow -translate-x-1/2 top-6 w-full max-w-5xl border-4 border-black bg-white py-3 px-4 rounded-full '
-        >
+        <header className='fixed z-50 left-1/2 custom-shadow -translate-x-1/2 top-4 sm:top-6
+            w-[calc(100%-2rem)] sm:w-[calc(100%-4rem)] max-w-5xl
+            border-4 border-black bg-white py-3 px-2 sm:px-4 rounded-full'>
 
-            <div
-                className="flex justify-between items-center"
-            >
+            <div className="flex justify-between items-center">
+
                 <button
                     onClick={() => navigate("/")}
                     className="flex cursor-pointer items-center gap-2"
                 >
-
-                    <div
-                        className="w-[3rem] h-auto"
-                    >
+                    <div className="w-6 sm:w-[3rem] h-auto">
                         <OptimizedImage
                             src={gitlensLogo}
                             alt="gitlens logo illustration"
                         />
                     </div>
-
-                    <h2
-                        className='text-black font-display font-extrabold text-2xl'
-                    >
+                    <h2 className='text-black font-display font-extrabold text-sm sm:text-2xl'>
                         gitLens
                     </h2>
                 </button>
 
                 <Button
                     onClick={() => navigate("/analyze")}
+                    className={cn(
+                        "flex justify-center items-center",
+                        "w-10 h-10 sm:w-auto sm:h-auto "
+                    )}
                 >
-                    <motion.span className="flex items-center gap-3">
+                    <motion.span className="flex justify-center items-center gap-2 sm:gap-3">
 
-                        <motion.span variants={leftIconVariants}>
+                        <motion.span className="hidden md:flex items-center justify-center" variants={leftIconVariants}>
                             <Analyze />
                         </motion.span>
 
-                        <span>Analyze Profile</span>
+                        <span className="hidden sm:flex justify-center items-center gap-3">Analyze Profile</span>
 
                         <motion.span variants={rightIconVariants}>
                             <Profile />
@@ -57,8 +55,8 @@ const Header = () => {
 
                     </motion.span>
                 </Button>
-            </div>
 
+            </div>
         </header>
     )
 }
